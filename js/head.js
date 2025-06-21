68f230adc60e3866025ceef88f20885ae9694eed
ydyy12138 <3038016818@qq.com>
2025-06-22 01:01:49 +0800

    Site updated: 2025-06-22 01:01:48

--- js/head.js
+++ js/head.js
@@ -4,3 +4,11 @@
     t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
     y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
 })(window, document, "clarity", "script", "s2zobwtjp9");
+
+
+// Google Analytics
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-J5FP75TJ7M');
